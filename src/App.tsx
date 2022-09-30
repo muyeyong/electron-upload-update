3bcf6958ec5c85c99b93a0246f83ab5160ee052f
muyeyong <xuyongshuaige@gmail.com>
2022-09-30 10:35:53 +0800

    feat: 新增格式化

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import { useState } from 'react'
 import styles from 'styles/app.module.scss'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0)
-  const uploadFile = () => {
-    postMessage({ payload: 'uploadFile' }, '*')
-  }
+    const [count, setCount] = useState(0)
+    const uploadFile = () => {
+        postMessage({ payload: 'uploadFile' }, '*')
+    }
 
-// 使用路由
-  return (
-    <div className={styles.app}>
-      <header className={styles.appHeader}>
-        {/* 触发选择文件 */}
-       <button onClick={uploadFile}>上传文件</button>
-       <Link to={'/check-upload'}>跳转到检查更新</Link>
-      </header>
-    </div>
-  )
+    // 使用路由
+    return (
+        <div className={styles.app}>
+            <header className={styles.appHeader}>
+                {/* 触发选择文件 */}
+                <button onClick={uploadFile}>上传文件</button>
+                <Link to={'/check-upload'}>跳转到检查更新</Link>
+            </header>
+        </div>
+    )
 }
 
 export default App
